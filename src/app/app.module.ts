a0b22a1920be64032d9d3529349985f14fc1e524
Daniel Jang <daniel.jang@programmer.net>
2021-05-08 13:40:04 -0400

    Add router-outlet / Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EmployeeModule } from '@module/employee/employee.module';
-
+import { RouterModule } from '@angular/router';
+import { EmployeeComponent } from '@module/employee/employee.component'
 @NgModule({
   declarations: [
     AppComponent
@@ -10,6 +11,10 @@ import { EmployeeModule } from '@module/employee/employee.module';
   imports: [
     BrowserModule,
     EmployeeModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/employee-list', pathMatch: 'full' },
+      { path: 'employee-list', component: EmployeeComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
